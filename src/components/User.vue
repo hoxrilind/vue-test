4b523318cc3c313e7fb786e5634f612c57870362
hoxrilind <rilind@centralsoft.io>
2022-02-15 15:06:59 +0100

    feat: create user form and temporary logic

---
+++ src/components/User.vue
@@ -0,0 +1,16 @@
+<template>
+<tr>
+    <td>{{ user.firstname }}</td>
+    <td>{{ user.lastname }}</td>
+    <td>{{ user.married }}</td>
+    </tr>
+</template>
+
+<script>
+export default {
+  name: "User",
+  props: {
+    user: Object,
+  },
+};
+</script>
